9fd120fdc8d28e2be6c00a666ba2c41cfa730ac1
rzinovyev <ruslan.zinovyev@hotmail.com>
2022-01-18 21:56:37 -0500

    implemented the video search

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,30 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../api/youtube";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
+  state = {
+    videos: [],
+  };
+
+  onSearchInputSubmit = async (searchInput) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: searchInput,
+      },
+    });
+
+    this.setState({
+      videos: response.data.items,
+    });
+  };
+
   render() {
     return (
-      <div>
-        <SearchBar />
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onSearchInputSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -1,8 +1,34 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { searchInput: "" };
+
+  onInputChange = (event) => {
+    this.setState({
+      searchInput: event.target.value,
+    });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onFormSubmit(this.state.searchInput);
+  };
+
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.searchInput}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
