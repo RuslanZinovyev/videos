5b9c23313603f25423cfbf5c3638b7d49cdaf358
rzinovyev <ruslan.zinovyev@hotmail.com>
2022-01-23 23:19:01 -0500

    final changes

--- src/component/VideoItem.js
+++ src/component/VideoItem.js
@@ -1,15 +1,17 @@
 import "./VideoItem.css";
 import React from "react";
 
-const VideoItem = (props) => {
+// Here we decompose props object with 2 properties video and onVideoSelect
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div className="video-item item">
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
       <img
+        alt={video.snippet.title}
+        src={video.snippet.thumbnails.medium.url}
         className="ui image"
-        src={props.video.snippet.thumbnails.medium.url}
       />
       <div className="content">
-        <div className="header">{props.video.snippet.title}</div>
+        <div className="header">{video.snippet.title}</div>
       </div>
     </div>
   );
